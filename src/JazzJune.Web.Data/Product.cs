2f8c8174f5112d71d44b77649c9aa3f50a6729c5
Melissa Houghton <57604950+melissahoughton@users.noreply.github.com>
2022-09-28 10:36:57 +0200

    Set Blazor Web Client

---
+++ src/JazzJune.Web.Data/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JazzJune.Web.Data;
+public class Product
+{
+    public Guid Id { get; set; } = Guid.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+}
